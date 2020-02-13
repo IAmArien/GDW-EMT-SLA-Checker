d3037f9a01ffb156c03d14036641a9e9c7c33d60
I Am Arien <45601866+IAmArien@users.noreply.github.com>
2020-02-13 16:06:57 +0800

    Configure Script

--- conf.py
+++ conf.py
@@ -1,12 +1,15 @@
 import sys as system
 import re as regex
+import uuid as unique_id
 from datetime import datetime
+from treelib import Node, Tree
 
 class Conf(object):
     def __init__(self, class_pst, class_email_conf):
         super().__init__()
         self.class_pst = class_pst
         self.class_email_conf = class_email_conf
+        self.tree = Tree()        
         
     def configure(self, config_file_dict: dict) -> dict:               
         config_file = self.class_email_conf.get_email_config_file(config_file_dict["Email-Monitoring"]["Config-Directory-Name"], config_file_dict["Email-Monitoring"]["Config-File-Extension"])
@@ -27,7 +30,7 @@ class Conf(object):
     def collect_valid_email_body(current_datetime: datetime, email_contents: dict, valid_emails_list: list, configuration_file: dict) -> tuple:        
         if email_contents["load_end_time"] == None:
             return ('Error', 'No Load End Time')
-        if datetime.now() >= email_contents["load_end_time"]:            
+        if current_datetime >= email_contents["load_end_time"]:            
             if email_contents["email"] in valid_emails_list:
                 models = configuration_file["Email-Monitoring"]["Accounts"][email_contents["email"]]["Models"]
                 __models__keys = {}
@@ -42,66 +45,107 @@ class Conf(object):
                                 __models__keys.update({"email-address": email_contents["email"]})
                                 __models__keys.update({"subject": email_contents["subject"]})
                                 __models__keys.update({"load_end_time": email_contents["load_end_time"]})
+                                __models__keys.update({"key-search": each_rules})
                                 __models__keys.update({each_controls: {start_end: regex.sub(r"(_x000D_|\n)", "", email_contents["body"])}})
                                 return ('Complete', __models__keys)
             else:
                 return ('Error', 'Invalid Email')
 
-    def validate_job_loads(self, container: list) -> dict:
+    def validate_job_loads(self, container: list) -> tuple:
         if container[0]["email-address"] == "GDW@ingrammicro.com":
             return self.load_gdw_configurations(container[0], container[1], "Dependencies", "Key-Sources")
 
     @staticmethod
-    def load_gdw_configurations(dict_logs: dict, configur_file: dict, depd_key: str, key_src: str) -> dict:
+    def load_gdw_configurations(dict_logs: dict, configur_file: dict, depd_key: str, key_src: str) -> tuple:
         if depd_key in dict_logs:
-            if key_src in dict_logs["Dependencies"]:
-                dependency_label = dict_logs["Dependencies"]["Key-Sources"]
+            if key_src in dict_logs[depd_key]:
+                dependency_label = dict_logs[depd_key][key_src]
                 em_add = dict_logs["email-address"]
                 let = dict_logs["load_end_time"]
-                subj = dict_logs["subject"]
-                del dict_logs["Dependencies"]
+                key_search = dict_logs["key-search"]                
+                #subj = dict_logs["subject"]
+                process = ""
+                hierarchy_structures = []
+                for each in dict_logs.keys():
+                    key_model = each                
+                for each_keys in dict(dict_logs[key_model]).keys():
+                    process = each_keys
+                hierarchy_structures.append(dependency_label)
+                hierarchy_structures.append(process)                
+                del dict_logs[depd_key]
                 del dict_logs["email-address"]
                 del dict_logs["subject"]
                 del dict_logs["load_end_time"]
+                del dict_logs["key-search"]
                 for each_models in dict_logs.keys():
                     for each_values in dict_logs[each_models]:
                         if each_values == depd_key:
                             continue
                         body_start_end = dict_logs[each_models][each_values]
                         start_end = each_values
+                        hierarchy_structures.append(body_start_end)
                         if dependency_label in body_start_end:
-                            runs = configur_file["Email-Monitoring"]["Accounts"][em_add]["GDW-Runs"].keys()                            
+                            runs = configur_file["Email-Monitoring"]["Accounts"][em_add]["Job-Runs"].keys()                            
                             ext_runs = ""
                             for each_runs in runs:
                                 if each_runs in body_start_end:
                                     ext_runs = each_runs
                                     break
-                            try:                                
-                                ks_dict = configur_file["Email-Monitoring"]["Accounts"][em_add]["Key-Sources"]
+                            try:
+                                hierarchy_structures.append(ext_runs)
+                                ks_dict = configur_file["Email-Monitoring"]["Accounts"][em_add][key_src]
                                 source = ks_dict[dependency_label]["Jobs"][start_end]
                                 for each_source in source:
                                     for each_run_nmbrs in source[each_source]:
                                         if source[each_source][each_run_nmbrs] == ext_runs:
                                             job_attrib = source[each_source][each_run_nmbrs]
                                             job_name = each_source
-                                gdw_runs = configur_file["Email-Monitoring"]["Accounts"][em_add]["GDW-Runs"][job_attrib][dependency_label][job_name]
+                                gdw_runs = configur_file["Email-Monitoring"]["Accounts"][em_add]["Job-Runs"][job_attrib][dependency_label][job_name]
                                 __email__struct = {}
                                 for each_time in gdw_runs:
                                     regex_str = regex.search(r"\d{4}-\d{2}-\d{2}", str(let))                                
                                     combine_str = "{date_re} {sla}".format(date_re=regex_str.group(), sla=gdw_runs[each_time])
                                     convert_datetime = datetime.strptime(combine_str, '%Y-%m-%d %H:%M:%S')
                                     __email__struct.update({"Job": job_name})
-                                    __email__struct.update({"Email-Notif": body_start_end})
-                                    __email__struct.update({"Subject": subj})
-                                    __email__struct.update({"Elements": each_time})
-                                    __email__struct.update({"Ave-Start-End": (let <= convert_datetime)})
-                                    __email__struct.update({"Ave-Start-End (Time)": gdw_runs[each_time]})
-                                    if not let <= convert_datetime:
-                                        if not each_time == "Start":
-                                            __email__struct.update({"SLA": False})
-                                            __email__struct.update({"SLA (Time)": gdw_runs[each_time]})                                        
-                                        return __email__struct
+                                    __email__struct.update({"Key Search": key_search})
+                                    __email__struct.update({"Datetime Received (PST)": let})
+                                    __email__struct.update({"Process": each_time})
+                                    __email__struct.update({"Average Start/End (Time)": gdw_runs[each_time]})
+                                    __email__struct.update({"Average Start/End (Bool)": (let <= convert_datetime)})
+                                    if not each_time == "SLA":
+                                        if let <= convert_datetime:
+                                            if each_time == "Start":
+                                                __email__struct.update({"SLA (Time)": "-----"})  
+                                                __email__struct.update({"SLA": "-----"})
+                                            else:
+                                                __email__struct.update({"SLA (Time)": gdw_runs[each_time]})                                                
+                                                __email__struct.update({"SLA": True})
+                                        else:
+                                            if each_time == "Start":
+                                                __email__struct.update({"SLA (Time)": "-----"})  
+                                                __email__struct.update({"SLA": "-----"})
+                                            else:
+                                                __email__struct.update({"SLA (Time)": gdw_runs[each_time]})                                  
+                                                __email__struct.update({"SLA": False})                                       
+                                        return (hierarchy_structures, __email__struct)
                             except KeyError as e:
                                 print(e)
-
-
+    
+    def hierarchy_structures(self, hierarchy_list: list, main_job: str) -> Tree:
+        try:
+            if not self.tree.contains(main_job):
+                self.tree.create_node(main_job, main_job)
+            try:
+                for each_h_list in hierarchy_list:
+                    if not self.tree.contains(each_h_list[0]):
+                        self.tree.create_node(each_h_list[0], each_h_list[0], parent=main_job)
+                    if not self.tree.contains("%s-%s" % (each_h_list[0], each_h_list[3])):
+                        self.tree.create_node(each_h_list[3], "%s-%s" % (each_h_list[0], each_h_list[3]), parent=each_h_list[0])
+                    if not self.tree.contains("%s-%s-%s" % (each_h_list[0], each_h_list[3], each_h_list[1])):
+                        self.tree.create_node(each_h_list[1], "%s-%s-%s" % (each_h_list[0], each_h_list[3], each_h_list[1]), parent="%s-%s" % (each_h_list[0], each_h_list[3]))
+                    self.tree.create_node(each_h_list[2], unique_id.uuid4(), parent="%s-%s-%s" % (each_h_list[0], each_h_list[3], each_h_list[1]))
+            except Exception as e:
+                print(e)
+        except Exception as e_1:
+            print(e_1)
+        return self.tree
