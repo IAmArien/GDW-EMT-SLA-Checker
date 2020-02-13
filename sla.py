b558e32b5fa5c1590e21cd2404d04d6058960cfa
I Am Arien <45601866+IAmArien@users.noreply.github.com>
2020-02-13 16:07:32 +0800

    Main Script

--- sla.py
+++ sla.py
@@ -2,6 +2,7 @@ import time as time
 import sys as system
 import json as javascript_notation
 import openpyxl as py_excel
+import win32com.client as win32
 from bind import Render_Excel
 from bind import Contents
 from bind import Email_Config
@@ -11,7 +12,8 @@ from os import path, listdir
 from os import getcwd, chdir
 from openpyxl import load_workbook
 from datetime import datetime
-from tabulate import tabulate
+from csv import writer
+from treelib import Node, Tree
 
 # HALT CONFIGURATION FILE PATH IF IT IS IN A PROPER 
 # FILE PATH FORMAT
@@ -47,9 +49,24 @@ def load_excel_work_book(file: str) -> object:
         return False
     return work_book
 
+def progressbar(it, prefix="", size=60, file=system.stdout):
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        file.write("%s%s%s %i/%i\r" % (prefix, "▐▌"*x, "."*(size-x), j, count))
+        file.flush()        
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    file.write("\n")
+    file.flush()
+
+
 # ---------------------------------------------------------------
-# START OF THE PROGRAM
-# Email Monitoring and SLA Checker Tool using Python 3.8
+# START OF THE PROGRAM# Email Monitoring and SLA Checker Tool using Python 3.8
+# == GLOBAL DATA WAREHOUSE ==
+# Project Supervisor: Erickson Flores
 # Developed By:
 # - Cabrera Troy A.
 # - Canolas Jevb John
@@ -62,7 +79,7 @@ chdir(path.dirname(path.realpath(__file__)))
 configuration_file_path = r"%s\%s" % (getcwd(), r"config\__config__.json")
 
 # # VALIDATE CONFIGURATION FILE
-# # * CHECK IF FILE EXISTS
+# * CHECK IF FILE EXISTS
 # * LOAD FILE IF ITS CONTENTS IS IN A JSON FORMAT
 order = halt_config(file_path=configuration_file_path)
 config_file = load_config_file(file_path=configuration_file_path, order=order)
@@ -76,6 +93,9 @@ excel_content = Render_Excel()
 configures = conf.configure(config_file)
 path_to_scan = config_file["Email-Monitoring"]["Path-To-Scan-Excels"]
 
+print(open(file=r"config\banner.txt", mode="r", encoding="utf-8").read())
+time.sleep(1)
+
 if not path.isdir(path_to_scan):
     print("DirectoryNotFoundError: No '%s' directory to scan, Please check __config__ file." % path_to_scan)
     system.exit("... Script Terminated Successfully ...")
@@ -86,7 +106,10 @@ for each_files in listdir(path=path_to_scan):
     work_book = load_excel_work_book(file=each_files)
     if work_book == False:
         continue
-    logs_array = []    
+    logs_array = []
+    hierarchy = []
+    headers=["Job", "Key Search", "Datetime Received (PST)", "Process", "Average Start/End (Time)", "Average Start/End (Bool)", "SLA (Time)", "SLA (Bool)"]
+    logs_array.append(headers) 
     temp_logs_arrays = []       
     for sheets in work_book.sheetnames:
         work_sheet = work_book[sheets]
@@ -94,14 +117,15 @@ for each_files in listdir(path=path_to_scan):
         if not content_checker.substantiate_email_contents(excel_columns):                                                    
             continue        
         key_count = 0
-        for rows in range(2, sum([work_sheet.max_row, 1])):                                                                                                                       
+        for rows in progressbar(range(2, sum([work_sheet.max_row, 1])), "%sChecking Emails: " % (chr(32)*4), 20):
             try:
                 valid_columns = config_file["Email-Monitoring"]["Email-Contents"]
                 email_address = excel_content.yield_email_address(work_sheet.cell(rows, excel_content.get_column_index(valid_columns[0], excel_columns)).value)
-                subject = str(work_sheet.cell(rows, excel_content.get_column_index(valid_columns[1], excel_columns)).value)
-                time_received = str(work_sheet.cell(rows, excel_content.get_column_index(valid_columns[2], excel_columns)).value)
-                body = work_sheet.cell(rows, excel_content.get_column_index(valid_columns[3], excel_columns)).value
-                load_end_time = excel_content.yield_load_end_time(body, email_address, config_file)
+                if email_address is not None:
+                    subject = str(work_sheet.cell(rows, excel_content.get_column_index(valid_columns[1], excel_columns)).value)
+                    time_received = str(work_sheet.cell(rows, excel_content.get_column_index(valid_columns[2], excel_columns)).value)
+                    body = work_sheet.cell(rows, excel_content.get_column_index(valid_columns[3], excel_columns)).value
+                    load_end_time = excel_content.yield_load_end_time(body, email_address, config_file)
             except ValueError as value_error:
                 # print("ValueError: %s" % value_error)
                 continue
@@ -109,15 +133,29 @@ for each_files in listdir(path=path_to_scan):
             log = conf.collect_valid_email_body(current_datetime=configures["standard_pt"], email_contents=email_contents, valid_emails_list=configures["emails"], configuration_file=config_file)
             if log != None:                
                 if log[0] == 'Complete':
-                    temp_array = []
+                    temp_array = []                    
                     dict_logs = conf.validate_job_loads([dict(log[1]), config_file])
                     if dict_logs != None:
+                        # print(dict_logs[0])
+                        hierarchy.append(dict_logs[0])
+                        dict_logs = dict_logs[1]
                         for each_temp in dict_logs:
                             temp_array.append(dict_logs[each_temp])
                         logs_array.append(temp_array)
-                  
-    with open("%s-logs.txt" % path.splitext(each_files)[0], "w+", encoding="utf-8") as output_logs:
-        output_logs.write(tabulate(logs_array, headers=["Job", "Email-Notification", "Subject", "Elements", "Average Start-End (Bool)", "Average Start-End (Time)", "SLA (Bool)", "SLA (Time)"], tablefmt='fancy_grid'))
-        output_logs.close()
+                        # outlook = win32.Dispatch('outlook.application')
+                        # mail = outlook.CreateItem(0)
+                        # mail.To = 'Troy.Cabrera@ingrammicro.com'
+                        # mail.Subject = temp_array[2]
+                        # mail.Body = temp_array[1]
+                        # mail.Send()                        
+            #time.sleep(0.00000000001)
+    with open("R_HIER_L_%s_LOGS.txt" % path.splitext(each_files)[0], "a+", newline='', encoding="utf-8") as hierarchy_logs:
+        hierarchy_logs.write(str(conf.hierarchy_structures(hierarchy, "GDWD0000")))
+        hierarchy_logs.close()    
+    with open("R_CSV_L_%s_LOGS.csv" % path.splitext(each_files)[0], "a+", newline='', encoding="utf-8") as output_logs:
+        csv_writer = writer(output_logs)
+        csv_writer.writerows(logs_array)
+    
+    time.sleep(3)
 
 
